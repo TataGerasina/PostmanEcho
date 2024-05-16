90979b74f2231e16cb93f1b74f1710750279df12
Tatyana Gerasina <gtstata@gmail.com>
2024-05-16 14:50:12 +0300

    Created project

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnPost() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data",equalTo("some data"))
+        ;
+    }
+}
